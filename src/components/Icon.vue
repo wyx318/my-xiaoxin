8d52cbf52e55a13384823747e27131bc1458d4fe
wyx318 <651033260@qq.com>
2020-04-16 23:47:22 +0800

    创建 <Icon />组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+	<svg class="icon">
+		<!--		或者这样写 :xlink:href="'#'+name" 一样的 不过下面的更优雅-->
+		<use :xlink:href="`#${name}`"></use>
+	</svg>
+</template>
+
+<script lang="ts">
+  // import x from '@/assets/icons/lable.svg';
+  // 统一引入该类型所有文件
+  let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons/', true, /\.svg$/));} catch (e) {console.log(e);}
+  export default {
+    props: ['name'],
+    name: 'Icon'
+  };
+</script>
+
+<style scoped lang="scss">
+	.icon {
+		width: 1em;height: 1em;
+		vertical-align: -0.15em;
+		fill: currentColor;
+		overflow: hidden;
+		}
+</style>
