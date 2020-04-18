cdb7b6dc8e95035fb1bd90f968ed706f8f9c328d
wyx318 <651033260@qq.com>
2020-04-18 16:39:12 +0800

    模块化 ：将Money.vue 分成四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<label class="notes">
+			<span class="name">备注</span>
+			<input type="text" placeholder="请输入">
+		</label>
+	</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Notes'
+  };
+</script>
+
+<style scoped lang="scss">
+	.notes {
+		background: #f5f5f5;
+		font-size: 14px;
+		display: flex;
+		align-items: center;
+		padding-left: 16px;
+		
+		.name {
+			padding-right: 16px;
+			}
+		
+		input {
+			height: 64px;
+			flex-grow: 1;
+			background: transparent; //变透明
+			border: none;
+			padding-right: 16px;
+			}
+		}
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,72 @@
+<template>
+	<div>
+		<div class="tags">
+			<div class="new">
+				<button>新增标签</button>
+			</div>
+			<ul class="current">
+				<li>衣</li>
+				<li>食</li>
+				<li>住</li>
+				<li>行</li>
+				<li>衣</li>
+				<li>食</li>
+				<li>住</li>
+				<li>行</li>
+				<li>衣</li>
+				<li>食</li>
+				<li>住</li>
+				<li>行</li>
+				<li>衣</li>
+				<li>食</li>
+				<li>住</li>
+				<li>行</li>
+			</ul>
+		</div>
+	
+	</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags'
+  };
+</script>
+
+<style scoped lang="scss">
+	.tags {
+		flex-grow: 1;
+		font-size: 14px;
+		padding: 16px;
+		display: flex;
+		flex-direction: column-reverse;
+		
+		> .current {
+			display: flex;
+			flex-wrap: wrap;
+			
+			> li {
+				background: #d9d9d9;
+				$h: 24px;
+				height: $h;
+				line-height: $h;
+				border-radius: $h/2;
+				padding: 0 16px;
+				margin-right: 12px;
+				margin-top: 4px;
+				}
+			}
+		
+		> .new {
+			padding-top: 16px;
+			
+			button {
+				background: transparent; //透明色
+				border: none;
+				border-bottom: 1px solid;
+				color: #999;
+				padding: 0 3px;
+				}
+			}
+		}
+</style>
