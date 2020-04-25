4c20ad3f0ee88d29ece91c380effb099210488f7
wyx318 <651033260@qq.com>
2020-04-25 18:52:24 +0800

    模块化的 store 删除了 models .

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,28 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-	//record store
-	recordList: recordListModel.fetch(),
-	createRecord: (record: RecordItem) => {recordListModel.create(record);},
-	//tag store
-	tagList: tagListModel.fetch(),
-	findTag(id: string) {
-		return this.tagList.filter(t => t.id === id)[0];
-	},
-	createTag: (name: string) => {
-		const message = tagListModel.create(name);
-		if (message === 'duplicated') {
-			window.alert('重复输入 ');
-		} else if (message === 'success') {
-			window.alert('添加成功 ');
-		}
-	},
-	removeTag: (id: string) => {
-		return tagListModel.remove(id);
-	},
-	updateTag: (id: string, name: string) => {
-		return tagListModel.update(id, name);
-	}
+  ...recordStore,
+  ...tagStore,
 };
+
+console.log(store);
+
 export default store;
