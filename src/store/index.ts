7c1e9e39cb8ccbde0a6ee14e8f6c044e79b93ddb
wyx318 <651033260@qq.com>
2020-04-27 16:25:15 +0800

    EditLabels.vue 使用 Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,13 +4,21 @@ import clone from '@/lib/clone';
 import createId from '@/lib/createId';
 
 Vue.use(Vuex); //把store 绑到 Vue.prototype 原型上
-const localStorageKeyName = 'recordList';
+type RootState = {
+	recordList: RecordItem[],
+	tagList: Tag[],
+	currentTag?: Tag
+}
 const store = new Vuex.Store({
 	state: { //data
-		recordList: [] as RecordItem[],
-		tagList: [] as Tag[]
-	},
+		recordList: [],
+		tagList: [],
+		currentTag: undefined
+	} as RootState,
 	mutations: { //methods
+		setCurrentTag(state, id: string) {
+			state.currentTag = state.tagList.filter(t => t.id === id)[0];
+		},
 		fetchRecords(state) {
 			state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
 		},
@@ -42,7 +50,7 @@ const store = new Vuex.Store({
 		},
 		saveTags(state) {
 			window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
-		}
+		},
 	}
 });
 export default store;
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -23,14 +23,16 @@
 	import Button from '@/components/Button.vue';
 
 	@Component({
-		components: { Button, FormItem }
+		components: { Button, FormItem },
 	})
 	export default class EditLabel extends Vue {
-		tag?: Tag = undefined;
+		get tag() {
+			return this.$store.state.currentTag;
+		}
 
 		created() {
-			//TODO
-			// this.tag = store.findTag(this.$route.params.id);
+			const id = this.$route.params.id;
+			this.$store.commit('setCurrentTag', id);
 			if (!this.tag) {
 				this.$router.replace('/404');
 			}
