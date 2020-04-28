f83bd201483fe6dd765656178268c0c99c0bd7a0
wyx318 <651033260@qq.com>
2020-04-28 23:28:34 +0800

    更新 css

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
-		<ul class="tabs">
+		<ul class="tabs" :class="{[classPrefix+'-tabs']: classPrefix}">
 			<li v-for="item in dataSource"
+					class="tabs-item"
 					:class="liClass(item)"
 					@click="select(item)"
 					:key="item.value">
@@ -41,7 +42,7 @@
 		text-align: center;
 		font-size: 24px;
 		
-		> li {
+		&-item {
 			width: 50%;
 			height: 64px;
 			display: flex;
